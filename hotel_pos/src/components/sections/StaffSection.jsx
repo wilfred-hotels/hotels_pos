116b17b2cbc0d7cf9e57f63f234e0a2917cef5f0
githaigakabuku <githaigakabuku@gmail.com>
2025-10-20 11:35:01 +0300

    so we made a dashboad used anime js but got some issues

--- hotel_pos/src/components/sections/StaffSection.jsx
+++ hotel_pos/src/components/sections/StaffSection.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 export default function StaffSection(){
+
+  const options = {
+  grid: [13, 13],
+  from: 'center',
+};
+
+createTimeline()
+  .add('.dot', {
+    scale: stagger([1.1, .75], options),
+    ease: 'inOutQuad',
+  }, stagger(200, options));
+
   return (
     <div>
       <h2 className="text-lg font-semibold mb-3">Staff</h2>
       <p className="text-sm text-gray-500">Staff directory and schedules.</p>
+      <div className=''></div>
+      
     </div>
   )
 }
